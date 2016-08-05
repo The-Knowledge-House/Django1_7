7fbcb3e3d7d4c385d12924d597dfa9011485b885
Joe C <joeknows718@Joes-MacBook-Pro.local>
2016-08-04 21:13:06 -0400

    part 2

--- TuTz718/urls.py
+++ TuTz718/urls.py
@@ -3,7 +3,8 @@ from TuTz718 import views
 
 
 urlpatterns = patterns('',
-	url(r'^$', views.index, name='index'))
+	url(r'^$', views.index, name='index'),
+	url(r'^about/', views.about, name="about"))
 
 
 
--- TuTz718/views.py
+++ TuTz718/views.py
@@ -3,5 +3,9 @@ from django.http import HttpResponse
 # Create your views here.
 
 def index(request):
-	return HttpResponse("Here will be tutorial")
+	context_dict = {'boldmessage':'tutorials here!'}
+	return render(request, 'index.html', context_dict)
+	
 
+def about(requrest):
+	return HttpResponse("About")
