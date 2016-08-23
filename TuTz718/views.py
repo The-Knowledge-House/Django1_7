180f69de18f959b783607b5983e92ceab09b9d13
Joe C <joeknows718@Joes-MacBook-Pro.local>
2016-08-23 15:13:32 -0400

    part 7 cookies

--- TuTz718/views.py
+++ TuTz718/views.py
@@ -3,16 +3,59 @@ from django.contrib.auth import authenticate, login, logout
 from django.http import HttpResponseRedirect, HttpResponse
 from models import Category, Page
 from forms import CategoryForm, PageForm, UserForm, UserProfileForm
+from django.contrib.auth.decorators import login_required
+from datetime import datetime
 # Create your views here.
 
 def index(request):
+	#request.session.set_test_cookie()
+
+
 	category_list = Category.objects.order_by('-likes')[:5]
 	page_list = Page.objects.order_by('-views')[:5]
 	context_dict = {'categories' : category_list,
 					'pages' : page_list
 					}
+	# If the cookie exists, the value returned is casted to an integer.
+    # If the cookie doesn't exist, we default to zero and cast that.
+	#visits =  int(request.COOKIES.get('visits', 1))
+	visits = request.session.get('visits')
+
+	if not visits:
+		visits = 1
+
+	reset_last_visit_time = False
+
+	#response = render(request, 'index.html', context_dict)
+	# Does the cookie last_visit exist?
+	last_visit = request.session.get('last_visit')
+	if last_visit:
+		# Yes it does Get the cookie's value.
+		#last_visit = request.COOKIES['last_visit']
+		#Cast the value to a Python date/time object.
+		last_visit_time = datetime.strptime(last_visit[:-7], "%Y-%m-%d %H:%M:%S")
+		# If it's been more than a day since the last visit increase by 1
+		if (datetime.now() - last_visit_time).days > 0:
+			visits = visits + 1
+			#flag cookie to be updated.
+			reset_last_visit_time = True 
+	else:
+		#Cookie last_visit doesn't exist, so flag that it should be set.
+		reset_last_visit_time = True
+		#context_dict['visits'] = visits
+		#Obtain our Response object early so we can add cookie information.
+		#response = render(request, 'index.html', context_dict)
+
+	if reset_last_visit_time:
+		#response.set_cookie('last_visit', datetime.now())
+		request.session['last_visit'] = str(datetime.now())
+		#response.set_cookie('visits', visits)
+		request.session[visits] = visits
+	context_dict['visits'] = visits
+
 	return render(request, 'index.html', context_dict)
-	
+
+
 
 def about(requrest):
 	return render(request, 'about.html', {})
@@ -82,7 +125,9 @@ def add_page(request, category_name_slug):
 def register(request):
 	#init bool to f change to t on success
 	registered = False
-
+	#if request.session.test_cookie_worked():
+	#	print '>>>Test Cookie Worked'
+	#	request.session.delete_test_cookie()
 	if request.method == 'POST':
 		user_form =  UserForm(data=request.POST)
 		profile_form =  UserProfileForm(data=request.POST)
@@ -151,8 +196,17 @@ def user_logout(request):
 	return HttpResponseRedirect('/')
 
 
+#@login_required
+#def restricted(request):
+#	return HttpResponse("Since you're logged in, you can see this text!")
 
 
+
+#def some_view(request):
+ #   if not request.user.is_authenticated():
+   #     return HttpResponse("You are logged in.")
+  #  else:
+    #    return HttpResponse("You are not logged in.")
 
 
 
--- tuts718/settings.py
+++ tuts718/settings.py
@@ -106,4 +106,4 @@ STATICFILES_DIRS = (
 )
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
-
+SESSION_COOKIE_AGE = 1209600
